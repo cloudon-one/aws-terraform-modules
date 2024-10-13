b51de12efa7caf2fa66bb3235a5e220a9f173237
ynaumenko <ynaumenko@outlook.com>
2024-10-13 16:17:13 +0300

    eks module upd

--- aws-terraform-eks/outputs.tf
+++ aws-terraform-eks/outputs.tf
@@ -1,14 +1,24 @@
-output "cluster_endpoints" {
-  description = "Endpoints of the EKS clusters"
-  value = [for cluster in aws_eks_cluster.this : cluster.endpoint]
+output "cluster_id" {
+  description = "The name/id of the EKS cluster"
+  value       = aws_eks_cluster.this.id
 }
 
-output "cluster_names" {
-  description = "Names of the EKS clusters"
-  value = [for cluster in aws_eks_cluster.this : cluster.name]
+output "cluster_arn" {
+  description = "The Amazon Resource Name (ARN) of the cluster"
+  value       = aws_eks_cluster.this.arn
 }
 
-output "node_group_arns" {
-  description = "ARNs of the EKS node groups"
-  value = [for ng in aws_eks_node_group.this : ng.arn]
+output "cluster_endpoint" {
+  description = "The endpoint for your EKS Kubernetes API"
+  value       = aws_eks_cluster.this.endpoint
+}
+
+output "cluster_security_group_id" {
+  description = "Security group ID attached to the EKS cluster"
+  value       = aws_eks_cluster.this.vpc_config[0].cluster_security_group_id
+}
+
+output "node_security_group_id" {
+  description = "Security group ID attached to the EKS nodes"
+  value       = var.node_security_group_name
 }
--- aws-terraform-eks/variables.tf
+++ aws-terraform-eks/variables.tf
@@ -1,37 +1,66 @@
-variable "region" {
-  description = "AWS region"
+variable "cluster_name" {
   type        = string
-  default     = ""
+  description = "Name of the EKS cluster"
 }
 
-variable "clusters" {
-  description = "List of EKS cluster configurations"
+variable "version" {
+  type        = string
+  description = "Kubernetes version"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID where the cluster and workers will be deployed"
+}
+
+variable "subnet_ids" {
+  type        = list(string)
+  description = "A list of subnet IDs where the nodes/node groups will be provisioned"
+}
+
+variable "iam_role_arn" {
+  type        = string
+  description = "IAM role ARN for the cluster"
+}
+
+variable "node_security_group_name" {
+  type        = string
+  description = "Name of the node security group"
+}
+
+variable "cluster_additional_security_group_ids" {
+  type        = list(string)
+  description = "List of additional, externally created security group IDs to attach to the cluster control plane"
+}
+
+variable "cluster_endpoint_public_access" {
+  type        = bool
+  description = "Indicates whether or not the Amazon EKS public API server endpoint is enabled"
+  default     = true
+}
+
+variable "eks_managed_node_groups" {
   type = list(object({
-    cluster_name = string
-    version = string
-    vpc_id = string
-    subnet_ids = list(string)
-    iam_role_arn = string
-    node_security_group_name = string
-    cluster_additional_security_group_ids = list(string)
-    cluster_endpoint_public_access = bool
-    eks_managed_node_groups = list(object({
-      name = string
-      instance_types = list(string)
-      min_size = number
-      max_size = number
-      desired_size = number
-      ami_type = string
-      capacity_type = string
+    name           = string
+    instance_types = list(string)
+    min_size       = number
+    max_size       = number
+    desired_size   = number
+    ami_type       = string
+    capacity_type  = string
+    access_entries = list(object({
+      principal          = string
+      type               = string
+      access_policies    = list(string)
+      kubernetes_groups  = list(string)
     }))
-    access_entries = optional(list(object({
-      principal = string
-      type = string
-      access_policies = optional(list(string))
-      kubernetes_groups = optional(list(string))
-      access = optional(string)
-    })))
-    tags = map(string)
+    tags           = map(string)
   }))
+  description = "List of managed node group configurations"
 }
 
+variable "tags" {
+  type        = map(string)
+  description = "A map of tags to add to all resources"
+  default     = {}
+}
