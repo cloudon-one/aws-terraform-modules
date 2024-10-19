1b641468ce02ab7b65d708808c5ada297c7c4042
ynaumenko <ynaumenko@outlook.com>
2024-10-19 12:15:34 +0300

    core tgw module refactor

--- aws-terraform-tgw/outputs.tf
+++ aws-terraform-tgw/outputs.tf
@@ -1,9 +1,24 @@
-output "transit_gateway_id" {
-  description = "ID of the created Transit Gateway"
-  value       = aws_ec2_transit_gateway.this.id
+output "tgw_id" {
+  description = "EC2 Transit Gateway identifier"
+  value       = module.tgw.ec2_transit_gateway_id
 }
 
-output "transit_gateway_attachment_id" {
-  description = "ID of the Transit Gateway VPC attachment"
-  value       = aws_ec2_transit_gateway_vpc_attachment.this.id
+output "tgw_arn" {
+  description = "EC2 Transit Gateway Amazon Resource Name (ARN)"
+  value       = module.tgw.ec2_transit_gateway_arn
+}
+
+output "tgw_route_table_id" {
+  description = "EC2 Transit Gateway Route Table identifier"
+  value       = module.tgw.ec2_transit_gateway_route_table_id
+}
+
+output "vpc_attachments" {
+  description = "Map of VPC attachments"
+  value       = module.tgw.vpc_attachments
+}
+
+output "ram_resource_share_id" {
+  description = "The ID of the resource share of TGW"
+  value       = module.tgw.ram_resource_share_id
 }
